99d2c32cf0c0266d3f8011241b4ddc9f9eb11a57
Kolyamba2007 <krupenko.ilya2000@gmail.com>
2021-12-07 15:17:04 +0300

    Update

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 settings = {
-    'token': 'OTE3MDk2NjgwNDcyNTk2NTEw.Yazu1w.X5Ob6Z5NuBDR4aaM8KIaL-VDOxs',
+    'token': 'OTE3MDk2NjgwNDcyNTk2NTEw.Yazu1w.6lHIhpRq32OkPxvNRFgT7rRW_DE',
     'bot': 'ASCII-converter',
     'id': 917096680472596510,
     'prefix': '!'
